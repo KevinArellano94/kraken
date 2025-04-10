460a738633e0f44b78f98b74c5261cb1b7494712
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2025-04-09 20:00:05 -0400

    testing

--- mod.ts
+++ mod.ts
@@ -1,4 +1,11 @@
 
+export function help(): string {
+  return `
+    help() - returns list of available commands
+    version() - returns the current version of the application
+  `;
+}
+
 export function getVersion(): string {
-  return "0.1.0";
+  return "0.0.0";
 }
